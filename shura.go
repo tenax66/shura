80948cd1df6ab46dc18c5918f574e195419f8d15
tenax66 <hiyoshir+tenax66@gmail.com>
2023-08-24 22:56:30 +0900

    add a prototype of the screenshot feature

--- shura.go
+++ shura.go
@@ -1,18 +1,56 @@
 package shura
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"math/rand"
 	"net/http"
+	"os"
 
+	"github.com/chromedp/chromedp"
 	"github.com/pkg/errors"
 )
 
 func Run() (string, error) {
 	address := generateOnionAddress()
+	// TODO: make this address mockable
+	// address := "https://example.com"
 
-	return fetchContent(address)
+	content, err := fetchContent(address)
+
+	if err != nil {
+		return content, err
+	}
+
+	takeScreenshot(address)
+
+	return content, nil
+}
+
+func takeScreenshot(address string) {
+	// create context
+	ctx, cancel := chromedp.NewContext(
+		context.Background(),
+	)
+	defer cancel()
+
+	var buf []byte
+
+	// capture entire browser viewport, returning png with quality=90
+	if err := chromedp.Run(ctx, fullScreenshot(address, 90, &buf)); err != nil {
+		// logging
+	}
+	if err := os.WriteFile("fullScreenshot.png", buf, 0o644); err != nil {
+		// logging
+	}
+}
+
+func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		chromedp.FullScreenshot(res, quality),
+	}
 }
 
 func fetchContent(address string) (string, error) {
