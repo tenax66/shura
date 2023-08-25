478c40b01edfd19e4f2be2fa8811cf162895dd56
tenax66 <hiyoshir+tenax66@gmail.com>
2023-08-25 21:44:48 +0900

    delete a redundant test

--- shura_test.go
+++ shura_test.go
@@ -5,28 +5,6 @@ import (
 	"testing"
 )
 
-func Test_fetchContent(t *testing.T) {
-	type args struct {
-		address string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{"normal", args{"https://example.com/"}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			_, err := fetchContent(tt.args.address)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("fetchContent() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-		})
-	}
-}
-
 func Test_tryAccess(t *testing.T) {
 	expected, _ := os.ReadFile("testdata/example_com.txt")
 
