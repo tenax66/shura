ab1e78e80027cf23a1f8ccbbb79c80620e34b024
tenax66 <hiyoshir+tenax66@gmail.com>
2023-08-25 21:44:11 +0900

    add unit test for tryAccess

--- shura_test.go
+++ shura_test.go
@@ -1,6 +1,9 @@
 package shura
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func Test_fetchContent(t *testing.T) {
 	type args struct {
@@ -19,6 +22,34 @@ func Test_fetchContent(t *testing.T) {
 			if (err != nil) != tt.wantErr {
 				t.Errorf("fetchContent() error = %v, wantErr %v", err, tt.wantErr)
 				return
+			}
+		})
+	}
+}
+
+func Test_tryAccess(t *testing.T) {
+	expected, _ := os.ReadFile("testdata/example_com.txt")
+
+	type args struct {
+		address string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"normal", args{"https://example.com/"}, string(expected), false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tryAccess(tt.args.address)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("tryAccess() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("tryAccess() = %v, want %v", got, tt.want)
 			}
 		})
 	}
